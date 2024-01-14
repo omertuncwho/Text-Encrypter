28e3da2f6fe0a79c001d570f66040e017d0911f9
Omer Tunc <omertuncwho@gmail.com>
2024-01-14 23:32:34 +0300

    Update app

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const nonCryptedText = document.getElementById('non-crypted-text');
     const cryptedText = document.getElementById('crypted-text');
-    const cryptButton = document.querySelector('.crypt');
+    const cryptButton = document.querySelector('.button');
 
     cryptButton.addEventListener('click', function () {
         const inputText = nonCryptedText.value;
